5fac62e933e97d617d0498c94e884972e3f0e7e5
Julie K <julieskim160@gmail.com>
2018-06-05 23:35:38 -0700

    add individual buttons

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { View, Image, Dimensions, Text, Button } from 'react-native'
 import Swiper from 'react-native-swiper'
+import { createStackNavigator } from 'react-navigation';
+
 const { width, height } = Dimensions.get('window');
 
 export default class extends Component {
@@ -8,30 +10,76 @@ export default class extends Component {
     return (
       <View style={styles.container}>
         
-        <Swiper style={styles.wrapper}
+        <Swiper 
+         //style={styles.wrapper}
           
-          dot={<View style={{backgroundColor: '#FEF9E7', width: 13, height: 13, borderRadius: 7, marginLeft: 7, marginRight: 7}} />}
-          activeDot={<View style={{backgroundColor: '#F9E79F', width: 13, height: 13, borderRadius: 7, marginLeft: 7, marginRight: 7}} />}
+          dot={<View style={{
+            backgroundColor: '#FEF9E7', 
+            width: 13, 
+            height: 13, 
+            borderRadius: 7, 
+            marginLeft: 7, 
+            marginRight: 7}} 
+          />}
+          activeDot={<View style={{
+            backgroundColor: '#F9E79F', 
+            width: 13, 
+            height: 13, 
+            borderRadius: 7, 
+            marginLeft: 7, 
+            marginRight: 7}} 
+          />}
           paginationStyle={{
-            bottom: 70
+            bottom: 100
           }}
           loop={false}>
           <View style={styles.slide}>
-            <Text style={styles.text}>soft</Text> 
          
+            <Text style={styles.text}>soft boil</Text> 
+            <Button 
+              title=">"
+              onPress={()=>console.log('hi')}/>
+              {/* style={styles.text}
+              title="soft"
+              onPress={() => console.log('hi')}>soft */}
+            
           </View>
           <View style={styles.slide}>
             <Text style={styles.text}>medium</Text> 
+            <Button 
+              title=">"
+              onPress={()=>console.log('hi')}/>
+            {/* <MediumEgg 
+              style={styles.text}
+              onPress={() => ('MediumEgg')}>
+              <Text style={styles.text}>medium</Text> 
+            </MediumEgg> */}
+          </View>
+          <View>
+            <Text style={styles.text}>hard</Text>
+            <Button 
+              title=">"
+              onPress={()=>console.log('hi')}/>
+          </View>
           
-          </View>
-          <View style={styles.slide}>
-            <Text style={styles.text}>hard</Text> 
+            {/* <Text style={styles.text}>hard</Text>  */}
             
-          </View>
+            {/* <Image
+              style={styles.image}
+              source={require('./img/2.jpg')}
+              resizeMode='cover'
+            /> */}
+         
 
         </Swiper>
-        <Button style={styles.button}
-          title=">"/>
+      
+        
+        {/* <Button 
+          // style={styles.button}
+          onPress={console.log("working")}
+          title="BUTTON"
+        /> */}
+      
       </View>
     )
   }
@@ -51,7 +99,15 @@ const styles = {
     textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 18,
-    marginTop: 300
+    marginTop: 200,
+  },
+  button: {
+    fontSize:50,
+    backgroundColor: 'blue'
+  },
+  image: {
+    width,
+    height,
   }
 }
 
